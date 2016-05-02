fb4ad083b3af086e140c842f9f7b68891b0d850f
Apoorv Verma [AP] <avdaredevil@gmail.com>
2016-05-01 20:19:30 -0400

    Results page uses AP magic :P

--- cla.py
+++ cla.py
@@ -1,7 +1,3 @@
-from Crypto.Signature import PKCS1_PSS as PKCS
-from Crypto.Hash import SHA
-from Crypto.PublicKey import RSA
-from base64 import b64encode, b64decode
 from flask import Flask, render_template, request, jsonify
 from OpenSSL import SSL
 import json, string, random, requests
@@ -12,8 +8,11 @@ context = ("keys/cfa.crt","keys/cfa.key")
 cla = Flask(__name__)
 
 # list of eligible voters
-eligible_voters = {"100":["Kartik", "Chopra", False]
-                 }
+eligible_voters = {
+    "100":["Kartik", "Chopra", False],
+    "111":["AP", "V", False],
+    "200":["John", "Doe", False]
+}
 @cla.route("/")
 def main():
     return render_template("cla_register.html")
@@ -27,46 +26,22 @@ def validation():
 @cla.route("/voter_name", methods=["POST"])
 def get_votername():
     if request.method == "POST":
-        signature = request.form["digsig"]
         valid_num = request.form["valid_num"]
         name = ""
-        if verify_dig_sig(signature, valid_num):
-            for voter in eligible_voters:
-                if eligible_voters[voter][2] == True and eligible_voters[voter][3] == valid_num:
-                    name = eligible_voters[voter][0] + " " + eligible_voters[voter][1]
-                    send_name(name)
-                    break
+        for voter in eligible_voters:
+            if eligible_voters[voter][2] == True and eligible_voters[voter][3] == valid_num:
+                name = eligible_voters[voter][0] + " " + eligible_voters[voter][1]
+                send_name(name)
+                break
     return "voter_name"
 
 def send_name(name):
-    signature = create_dig_sig(name)
-    info = {"digsig":signature,"name":name}
+    info = {"name":name}
     req = requests.post("https://localhost:8090/get_votername", data=info, verify=False)
 
 def send_valid_num(validation_num):
-    signature = create_dig_sig(validation_num)
-    info = {"digsig":signature,"valid_num":validation_num}
+    info = {"valid_num":validation_num}
     req = requests.post("https://localhost:8090/add", data=info, verify=False)
-
-def create_dig_sig(message):
-    f = open("./keys/rsa", "r") # get private key
-    key = RSA.importKey(f.read())
-    h = SHA.new()
-    h.update(message)
-    signer = PKCS.new(key)
-    signature = signer.sign(h)
-    return b64encode(signature)
-
-def verify_dig_sig(signature, message):
-    f = open("./keys/ctf_rsa.pub", "r") # get ctf's public key
-    key = RSA.importKey(f.read())
-    h = SHA.new()
-    h.update(message)
-    verifier = PKCS.new(key)
-    if verifier.verify(h, b64decode(signature)):
-        return True
-    else:
-        return False
 
 def generate_valid_num(length):
     lst = [random.choice(string.ascii_letters + string.digits)
--- ctf.py
+++ ctf.py
@@ -1,9 +1,5 @@
-from base64 import b64encode, b64decode
 from flask import Flask, render_template, request, jsonify
 from OpenSSL import SSL
-from Crypto.Signature import PKCS1_PSS as PKCS
-from Crypto.Hash import SHA
-from Crypto.PublicKey import RSA
 import string, random, requests
 
 # SSL setup
@@ -27,10 +23,8 @@ def confirmation():
 @ctf.route("/get_votername", methods=["POST"])
 def get_votername():
     if request.method == "POST":
-        signature = request.form["digsig"]
         name = request.form["name"]
-        if verify_dig_sig(signature, name):
-            names.append(name)
+        names.append(name)
     return "get_votername"
 
 @ctf.route("/")
@@ -40,11 +34,9 @@ def main():
 @ctf.route("/add", methods=["POST"])
 def add():
     if request.method == "POST":
-        signature = request.form["digsig"]
         valid_num = request.form["valid_num"]
         verified = False
-        if verify_dig_sig(signature, valid_num):
-            valid_nums[valid_num] = False
+        valid_nums[valid_num] = False
     return "add"
 
 @ctf.route("/results")
@@ -52,29 +44,8 @@ def display_results():
     return render_template("ctf_results.html", voters=voters, votes=votes, names=names)
 
 def request_voter_name(valid_num):
-    signature = create_dig_sig(valid_num)
-    info = {"digsig":signature , "valid_num":valid_num}
+    info = {"valid_num":valid_num}
     req = requests.post("https://localhost:8091/voter_name", data=info, verify=False)
-
-def create_dig_sig(message):
-    f = open("./keys/ctf_rsa", "r") # get ctf's private key
-    key = RSA.importKey(f.read())
-    h = SHA.new()
-    h.update(message)
-    signer = PKCS.new(key)
-    signature = signer.sign(h)
-    return b64encode(signature)
-
-def verify_dig_sig(signature, message):
-    f = open("./keys/rsa.pub", "r") # get cla's public key
-    key = RSA.importKey(f.read())
-    h = SHA.new()
-    h.update(message)
-    verifier = PKCS.new(key)
-    if verifier.verify(h, b64decode(signature)):
-        return True
-    else:
-        return False
 
 def validate_voter(rand_id, valid_num, vote):
     allowed = False
